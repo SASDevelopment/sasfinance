c677d57ea89b1871d2e3769fcd3879fb6af880bd
tmccrary-sas <tmccrary@christianpublishing.com>
2017-09-25 14:38:45 -0400

    cleared test files

--- assets/cron/royalties3_load_currency.php
+++
@@ -1,4 +0,0 @@
-<?php
-include($_SERVER['DOCUMENT_ROOT'].'/sasfinance/assets/functions.royalties3.php');
-royalties3_load_royalty_currency_data();
-?>
--- testrows.php
+++
@@ -1,4 +0,0 @@
-<?
-include_once('assets/functions.royalties3.php'); 
-echo royalties3_display_import_exceptions('temp_ebook_amazon');
-?>
